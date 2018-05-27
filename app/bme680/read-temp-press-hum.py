61fb213a1898db6067576f5f6011c9efa45dd16f
Piyamate Wisanuvej <piyamate.w12@imperial.ac.uk>
2018-05-27 02:11:47 +0100

    add support for ds18b20 and bme680

---
+++ app/bme680/read-temp-press-hum.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import bme680
+
+sensor = bme680.BME680()
+
+# These oversampling settings can be tweaked to
+# change the balance between accuracy and noise in
+# the data.
+
+sensor.set_humidity_oversample(bme680.OS_2X)
+sensor.set_pressure_oversample(bme680.OS_4X)
+sensor.set_temperature_oversample(bme680.OS_8X)
+sensor.set_filter(bme680.FILTER_SIZE_3)
+
+if sensor.get_sensor_data():
+        output = "{0:.2f} C,{1:.2f} hPa,{2:.3f} %RH".format(sensor.data.temperature, sensor.data.pressure, sensor.data.humidity)
+
+print(output)
